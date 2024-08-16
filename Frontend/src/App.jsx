24192d54d687cca8f195b29ba7b8a6b1bc5df9f3
Vatsalkayastha <vatsal.k2@ahduni.edu.in>
2024-08-16 10:04:56 +0530

    Home Page Ready

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home"
+import Home from "./pages/Home.jsx"
+import Form from './pages/Form.jsx';
+
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/form' element={<Form />} />
       </Routes>
     </Router>
   )
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import logo from "../assets/google-forms.png";
+import plus from "../assets/plus.svg";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div className=''>Home</div>
-  )
-}
+    <div className="w-full h-100vh">
+      <nav className="flex">
+        <img src={logo} alt="Logo" className="w-12 h-12 mr-5 my-3 mx-10" />
+        <div className="flex my-5 text-3xl text-black">Google Forms</div>
+      </nav>
+      <div className="bg-gray-400 w-full h-48 flex">
+        <Link to="/form">
+          <div className="w-40 h-40 bg-white rounded-xl border mx-4 px-3 py-3 mt-3 justify-center items-center">
+            <img
+              src={plus}
+              alt=""
+              className="justify-center m-auto mt-5 cursor-pointer"
+            />
+            <div className="font-bold mx-auto px-5"> Create Form </div>
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
